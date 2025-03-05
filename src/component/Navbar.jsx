24bd063100e24d5aa99b4581f2f89b4da1db4977
unknown <youloos477@gmail.com>
2025-03-05 12:17:40 +0500

    Code Update

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -20,7 +20,7 @@ const Navbar = () => {
     return () => window.removeEventListener("scroll", handleScroll);
   }, []);
   return (
-    <nav className={`sticky-top navbar navbar-expand-lg custom-navbar ${scrolled ? "navbar-scrolled" : ""}`}>
+    <nav className={`navbar navbar-expand-lg custom-navbar ${scrolled ? "navbar-scrolled" : ""}`}>
       <div className="container">
         <Link className="navbar-brand fw-bold text-dark" to="/">
           Boot<span className="text-primary">Press</span>
